f6c65cd9fda56cc33d399d8f565ec9bc57f34650
rhino44 <batyarthur@gmail.com>
2016-04-18 12:19:53 +0200

    init
    nodewebserverinit

---
+++ nodeWebServer/indexturn.js
@@ -0,0 +1,39 @@
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var arduinosocket;
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	console.log('connected');
+	socket.on('iamarduino', function(ok){
+		arduinosocket = socket;
+		console.log("arduino connected");
+		arduinosocket.on('sensor', function(data){
+
+			console.log(data);
+			io.emit('sensor', data);
+		});
+	});
+
+	socket.on('turn', function(ok){
+		arduinosocket.emit('turn', 'ok');
+		console.log('turn');
+	});
+	
+
+});
+
+
+
+http.listen(8084, 'localhost', function () {
+
+	console.log('ok');
+
+});
