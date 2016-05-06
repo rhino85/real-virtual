21d135786b9ac31d6600b4f12b99ee3672c5251b
rhino85 <batyarthur@gmail.com>
2016-05-06 14:51:22 +0200

    fixed socket io duplicating messages

--- nodeClientArduino/index.js
+++ nodeClientArduino/index.js
@@ -1,32 +1,46 @@
 var io = require('socket.io-client');
 var socket = io.connect("http://arduino.rhinocerosdu85.com");
 var serialport = require("serialport");
-
+var n = 0;
+var socketConnected = false;
+var serialConnected = false;
 var Serialport = new serialport.SerialPort("COM3", {
 		parser: serialport.parsers.readline("\n")
 	}, false); 
+
 socket.on('connect', function(ok){
 
 	console.log("connected");
 	socket.emit('iAmArduino', 'hi');
-	Serialport.on('data', function(data){
+});
 
-		socket.emit('sensor', data);
-	});
+Serialport.open(function(){
+		
+	console.log('Serial Port Opend');
+	serialConnected = true;
 });
 
 socket.on('hiArduino', function(ok){
 
-	Serialport.open(function(){
-		console.log('Serial Port Opend');
-
-	});
-
+	socketConnected = true;
 });
 
-socket.on('turn', function(ok){
+Serialport.on('data', function(data){
+	
+	if(socketConnected){
+		n++;
+		console.log(n + " " + data)
+		socket.emit('sensor', data);
+	}
+});
 
-	Serialport.write(1);
-	console.log('turn');
+
+
+socket.on('turn', function(ok){
+	
+	if(serialConnected){
+		Serialport.write(1);
+		console.log('turn');
+	}
 });
 
